d16e7a36b4609e5a174dc022292dd9cc295f3f24
MessonPimentel <PimentelSouza@users.noreply.github.com>
2022-08-10 16:17:46 -0300

    empilhar,desempilhar, incluir cenarios

--- src/Pilha.java
+++ src/Pilha.java
@@ -1,35 +1,39 @@
 
 public class Pilha {
-	
-	
-	private Object[] elementos = new Object[10];
-	private int quantidade = 0;
-	
 
+	private Object[] elementos;
+	private int quantidade = 0;
+
+	public Pilha(int maximo) {
+		elementos = new Object[maximo];
+	}
 
 	public boolean estaVazia() {
-		
+
 		return quantidade == 0;
 	}
-	
+
 	public int tamanho() {
-		
+
 		return quantidade;
 	}
-	
 
 	public void empilha(Object elemento) {
+		if (quantidade == elementos.length)
+			throw new PilhaCheiaException("Não e possível empilhar mais elementos.");
 		this.elementos[quantidade] = elemento;
 		quantidade++;
-		
+
 	}
 
 	public Object topo() {
-		
-		return elementos[quantidade-1];
+
+		return elementos[quantidade - 1];
 	}
 
 	public Object desempilha() {
+		if (estaVazia())
+			throw new PilhaVaziaException("Não é possível desempilhar.");
 		Object topo = topo();
 		quantidade--;
 		return topo;
--- src/TestePilha.java
+++ src/TestePilha.java
@@ -1,12 +1,21 @@
 import static org.junit.jupiter.api.Assertions.*;
 
+import org.junit.Before;
 import org.junit.jupiter.api.Test;
 
-class TestePilha {
+public class TestePilha {
+
+	private Pilha p;
+
+	@Before
+	public void inicializaPilha() {
+
+		p = new Pilha(10);
+
+	}
 
 	@Test
-	public void test() {
-		Pilha p = new Pilha();
+	public void pilhaVazia() {
 		assertTrue(p.estaVazia());
 		assertEquals(0, p.tamanho());
 
@@ -14,16 +23,14 @@ class TestePilha {
 
 	@Test
 	public void empilhaUmElemnto() {
-		Pilha p = new Pilha();
 		p.empilha("primeiro");
 		assertFalse(p.estaVazia());
 		assertEquals(1, p.tamanho());
 		assertEquals("primeiro", p.topo());
 	}
-	
+
 	@Test
 	public void empilhaEDesempilha() {
-		Pilha p = new Pilha();
 		p.empilha("primeiro");
 		p.empilha("segundo");
 		assertEquals(2, p.tamanho());
@@ -32,6 +39,29 @@ class TestePilha {
 		assertEquals(1, p.tamanho());
 		assertEquals("primeiro", p.topo());
 		assertEquals("segundo", desempilhado);
+	}
+
+	// É preciso no TDD incluir cenários em que a classe deve lançar
+
+	@Test(expected = PilhaVaziaException.class)
+	public void removeDaPilhaVazia() {
+		p.desempilha();
+
+	}
+
+	@Test
+	public void adicionaNaPilhaCheia() {
+		for (int i = 0; i < 10; i++) {
+			p.empilha("elemento" + i);
+		}
+
+		try {
+			p.empilha("boom");
+			fail();
+		} catch (PilhaCheiaException e) {
+
+		}
+
 	}
 
 }

0822614dc52c4fc9a99d18399d430f24fe2a009e
MessonPimentel <PimentelSouza@users.noreply.github.com>
2022-08-10 13:53:34 -0300

    Subir

---
+++ src/TestePilha.java
@@ -0,0 +1,37 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class TestePilha {
+
+	@Test
+	public void test() {
+		Pilha p = new Pilha();
+		assertTrue(p.estaVazia());
+		assertEquals(0, p.tamanho());
+
+	}
+
+	@Test
+	public void empilhaUmElemnto() {
+		Pilha p = new Pilha();
+		p.empilha("primeiro");
+		assertFalse(p.estaVazia());
+		assertEquals(1, p.tamanho());
+		assertEquals("primeiro", p.topo());
+	}
+	
+	@Test
+	public void empilhaEDesempilha() {
+		Pilha p = new Pilha();
+		p.empilha("primeiro");
+		p.empilha("segundo");
+		assertEquals(2, p.tamanho());
+		assertEquals("segundo", p.topo());
+		Object desempilhado = p.desempilha();
+		assertEquals(1, p.tamanho());
+		assertEquals("primeiro", p.topo());
+		assertEquals("segundo", desempilhado);
+	}
+
+}
